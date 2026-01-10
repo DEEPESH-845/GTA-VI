c2f2f45890bbcbdafbac5e860f4984df97c9310b
DEEPESH-845 <dkakkar_be23@thapar.edu>
2026-01-10 21:30:55 +0530

    Added GSAP

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
+
+
+gsap.registerPlugin(ScrollTrigger);
+
+const App = () => {
+	return <main></main>;
+};
+
+export default App;
